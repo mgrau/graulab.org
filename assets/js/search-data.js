1b84126adda87fd9b8f37d1d5d82ab0c5b623143
mgrau <mgrau@users.noreply.github.com>
2026-03-18 14:07:18 +0000

    Deploying to gh-pages from @ mgrau/graulab.org@5bfc8706f067ec4dc67d778fee2e3ce62d01874e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-people",
           title: "people",
-          description: "Current group members and alumni.",
+          description: "Grau Lab current group members and alumni.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
